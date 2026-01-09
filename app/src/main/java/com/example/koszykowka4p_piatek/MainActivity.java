2f57a74a709e1168c1f4fa6339665034ff53792a
mechaniktgmobilne <marzkup@gmail.com>
2026-01-09 11:09:40 +0100

    dzialanie aplikacji

--- app/src/main/java/com/example/koszykowka4p_piatek/MainActivity.java
+++ app/src/main/java/com/example/koszykowka4p_piatek/MainActivity.java
@@ -3,12 +3,49 @@ package com.example.koszykowka4p_piatek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textViewP;
+    private Button butoon1, button2,button3;
+    private int punkty;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewP = findViewById(R.id.textViewPunkty);
+        butoon1 = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        textViewP.setText(String.valueOf(punkty));
+        butoon1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty++;
+                        textViewP.setText(String.valueOf(punkty));
+                    }
+                }
+        );
+        button2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty+=2;
+                        textViewP.setText(String.valueOf(punkty));
+                    }
+                }
+        );
+        button3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty+=3;
+                        textViewP.setText(String.valueOf(punkty));
+                    }
+                }
+        );
     }
 }
